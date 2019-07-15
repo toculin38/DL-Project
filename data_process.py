5ce5e30cc8878263a0c85eccdbade4fdaed46ff8
toculin38 <toculin38@gmail.com>
2019-07-15 17:51:46 +0800

    Final Ver.

--- data_process.py
+++ data_process.py
@@ -14,34 +14,29 @@ KeyToPitch[0] = 0
 
 KeySize = len(PitchTokey)
 PressSize = int(midi_util.OffsetMax / midi_util.OffsetStep) # Number of offset in a bar
-CycleTimes = 8
+CycleTimes = 16
 CycleLength = PressSize * CycleTimes # Represent a Cycle of music pattern
 OffsetStep = midi_util.OffsetStep
 OffsetBitSize = int(np.log2(CycleLength))
-def prepare_sequences(data, sequence_length, modify_num=0):
+
+def prepare_melody_sequences(data, sequence_length, modify_num=0):
     keys_input = []
     press_input = []
     offset_input = []
     keys_output = []
     press_output = []
-    keys2_output = []
-    press2_output = []
     
     # create input sequences and the corresponding outputs
     for notes in data:
         key_sequence = np.array([PitchTokey[note[0]] for note in notes])
         press_sequence = np.array([note[1] - 1 for note in notes])
-        key2_sequence = np.array([PitchTokey[note[2]] for note in notes])
-        press2_sequence = np.array([note[3] - 1 for note in notes])
         offset_sequence = np.unpackbits(np.array([[idx % CycleLength] for idx, note in enumerate(notes)], dtype=np.uint8), axis=-1)[:,-OffsetBitSize:]
 
         for i in range(0, len(notes) - 2 * sequence_length, 1):
             key_sequence_in = key_sequence[i:i + sequence_length]
             key_sequence_out = key_sequence[i + sequence_length: i + 2 * sequence_length]
-            key2_sequence_out = key2_sequence[i:i + sequence_length]
             press_sequence_in = press_sequence[i:i + sequence_length]
             press_sequence_out = press_sequence[i + sequence_length : i + 2 * sequence_length]
-            press2_sequence_out = press2_sequence[i:i + sequence_length]
             offset_sequence_in = offset_sequence[i:i + sequence_length]
 
             for _ in range(modify_num):
@@ -52,10 +47,8 @@ def prepare_sequences(data, sequence_length, modify_num=0):
 
             keys_input.append(key_sequence_in)
             keys_output.append(key_sequence_out)
-            keys2_output.append(key2_sequence_out)
             press_input.append(press_sequence_in)
             press_output.append(press_sequence_out)
-            press2_output.append(press2_sequence_out)
             offset_input.append(offset_sequence_in)
 
     # reshape the input into a format compatible with LSTM layers
@@ -71,12 +64,61 @@ def prepare_sequences(data, sequence_length, modify_num=0):
     press_output = to_onehot(np.array(press_output), PressSize)
     press_output = np.reshape(press_output, (n_patterns , sequence_length, PressSize))
 
+    return [keys_input, press_input, offset_input], [keys_output, press_output]
+
+def prepare_accomp_sequences(data, sequence_length, modify_num=0):
+    keys_input = []
+    press_input = []
+    offset_input = []
+    keys2_output = []
+    press2_output = []
+    accomp_input = []
+
+    # create input sequences and the corresponding outputs
+    for notes in data:
+        key_sequence = np.array([PitchTokey[note[0]] for note in notes])
+        press_sequence = np.array([note[1] - 1 for note in notes])
+        key2_sequence = np.array([PitchTokey[note[2]] for note in notes])
+        press2_sequence = np.array([note[3] - 1 for note in notes])
+        offset_sequence = np.unpackbits(np.array([[idx % CycleLength] for idx, note in enumerate(notes)], dtype=np.uint8), axis=-1)[:,-OffsetBitSize:]
+
+        for i in range(sequence_length, len(notes) - sequence_length, 1):
+            key_sequence_in = key_sequence[i:i + sequence_length]
+            key2_sequence_out = key2_sequence[i:i + sequence_length]
+            press_sequence_in = press_sequence[i:i + sequence_length]
+            press2_sequence_out = press2_sequence[i:i + sequence_length]
+            offset_sequence_in = offset_sequence[i:i + sequence_length]
+            accomp_sequence_in = key2_sequence[i - sequence_length: i]
+
+            for _ in range(modify_num):
+                key_sequence_in, press_sequence_in = random_modify(key_sequence_in, press_sequence_in)
+
+            for _ in range(modify_num):
+                key2_sequence_out, press2_sequence_out = random_modify(key2_sequence_out, press2_sequence_out)
+
+            keys_input.append(key_sequence_in)
+            keys2_output.append(key2_sequence_out)
+            press_input.append(press_sequence_in)
+            press2_output.append(press2_sequence_out)
+            offset_input.append(offset_sequence_in)
+            accomp_input.append(accomp_sequence_in)
+
+    # reshape the input into a format compatible with LSTM layers
+    n_patterns = len(keys_input)
+    keys_input = to_onehot(np.array(keys_input), KeySize)
+    keys_input = np.reshape(keys_input, (n_patterns, sequence_length, KeySize))
+    press_input = to_onehot(np.array(press_input), PressSize)
+    press_input = np.reshape(press_input, (n_patterns, sequence_length, PressSize))
+    offset_input = np.reshape(offset_input, (n_patterns, sequence_length, OffsetBitSize))
+    accomp_input = to_onehot(np.array(accomp_input), KeySize)
+    accomp_input = np.reshape(accomp_input, (n_patterns, sequence_length, KeySize))
+
     keys2_output = to_onehot(np.array(keys2_output), KeySize)
     keys2_output = np.reshape(keys2_output, (n_patterns, sequence_length, KeySize))
     press2_output = to_onehot(np.array(press2_output), PressSize)
     press2_output = np.reshape(press2_output, (n_patterns , sequence_length, PressSize))
 
-    return [keys_input, press_input, offset_input], [keys_output, press_output], [keys2_output, press2_output]
+    return [keys_input, press_input, offset_input, accomp_input], [keys2_output, press2_output]
 
 def to_onehot(array, num_classes):
     return np_utils.to_categorical(array, num_classes=num_classes) 
@@ -85,7 +127,7 @@ def random_modify(key_sequence, press_sequence):
     key_sequence = key_sequence.copy()
     press_sequence = press_sequence.copy()
     random_index = np.random.randint(0, key_sequence.shape[0])
-    random_key = np.clip(key_sequence[random_index] + np.random.randint(-4, 5), 0, KeySize - 1)
+    random_key = np.clip(key_sequence[random_index] + np.random.randint(-2, 3) * 2, 0, KeySize - 1)
     key_sequence[random_index] = random_key
 
     press = press_sequence[random_index]
@@ -99,43 +141,53 @@ def random_modify(key_sequence, press_sequence):
 
     return key_sequence, press_sequence
 
-def random_pattern(data):
+def random_pattern(data, frequency=0.5):
     key_pattern = np.zeros(data[0].shape[1])
     press_pattern = np.zeros(data[1].shape[1])
+    accomp_pattern = np.zeros(data[3].shape[1])
 
     for idx, _ in enumerate(zip(key_pattern, press_pattern)):
-        if idx != 0 and np.random.uniform(0, 1) < 0.75:
+        if idx != 0 and np.random.uniform(0, 1) > frequency:
             press_pattern[idx] = (press_pattern[idx - 1] + 1) % PressSize
             key_pattern[idx] = key_pattern[idx - 1]
         else:
             press_pattern[idx] = 0
-            key_pattern[idx] = np.random.randint(13, KeySize - 12)
+            key_pattern[idx] = np.random.randint(0, KeySize)
+
+        accomp_pattern[idx] = np.random.randint(0, KeySize)
 
     offset_pattern = data[2][0]
-    return key_pattern, press_pattern, offset_pattern
+    return key_pattern, press_pattern, offset_pattern, accomp_pattern
+
+def random_pattern_from_data(data):
+    start = (np.random.randint(0, data[0].shape[0]) // data[0].shape[1]) * data[0].shape[1]
+    print(start)
+    key_pattern = np.argmax(data[0][start], axis=-1)
+    prs_pattern = np.argmax(data[1][start], axis=-1)
+    ofs_pattern = data[2][0]
+    acp_pattern = np.argmax(data[3][start], axis=-1)
+    return key_pattern, prs_pattern, ofs_pattern, acp_pattern
 
 def generate_notes(melody_model, accomp_model, data):
-    key_pattern , prs_pattern, offset_pattern = random_pattern(data)
+    # key_pattern , prs_pattern, ofs_pattern, acp_pattern = random_pattern(data)
+    key_pattern , prs_pattern, ofs_pattern, acp_pattern = random_pattern_from_data(data)
+
     print(key_pattern)
     print(prs_pattern)
 
     melody_output = []
     accomp_output = []
-
-    # generate 64 bars
-    for _ in range(32):
+    # generate 16 * SequenceLength notes
+    for _ in range(16):
         # random modify the pattern to prevent looping
-        copy_key_pattern, copy_prs_pattern = random_modify(key_pattern, prs_pattern)
-
+        # copy_key_pattern, copy_prs_pattern = random_modify(key_pattern, prs_pattern)
         # predict melody
         key_input = np_utils.to_categorical(key_pattern, num_classes=KeySize)
         key_input = np.reshape(key_input, (1, key_input.shape[0], key_input.shape[1]))
         prs_input = np_utils.to_categorical(prs_pattern, num_classes=PressSize)
         prs_input = np.reshape(prs_input, (1, prs_input.shape[0], prs_input.shape[1]))
-        offset_input = np.reshape(offset_pattern, (1, offset_pattern.shape[0], offset_pattern.shape[1]))
-        
-        prediction_input = [key_input, prs_input, offset_input]
-        prediction = melody_model.predict(prediction_input, verbose=0)
+        ofs_input = np.reshape(ofs_pattern, (1, ofs_pattern.shape[0], ofs_pattern.shape[1]))
+        prediction = melody_model.predict([key_input, prs_input, ofs_input], verbose=0)
         key_prediction = np.reshape(prediction[0], (prediction[0].shape[1], prediction[0].shape[2]))
         prs_prediction = np.reshape(prediction[1], (prediction[1].shape[1], prediction[1].shape[2]))
 
@@ -143,41 +195,40 @@ def generate_notes(melody_model, accomp_model, data):
             key_index = np.argmax(key_onehot)
             prs_index = np.argmax(prs_onehot)
             melody_output.append((KeyToPitch[key_index], prs_index))
-            # adjust pattern
             key_pattern[index] = key_index
             prs_pattern[index] = prs_index
-
+            
         # adjust offset pattern
-        shift_offset = len(offset_pattern)
-        new_offset_pattern = np.packbits(offset_pattern, axis=-1) // (2 ** (8 - OffsetBitSize)) # pack to int
+        shift_offset = len(ofs_pattern)
+        new_offset_pattern = np.packbits(ofs_pattern, axis=-1) // (2 ** (8 - OffsetBitSize)) # pack to int
         new_offset_pattern = (new_offset_pattern + shift_offset) % CycleLength # shift
-        new_offset_pattern = np.unpackbits(new_offset_pattern, axis=-1) # unpack to 8 bits
-        offset_pattern = new_offset_pattern[:,-OffsetBitSize:] # select indices 1 ~ 7 bits (discard 0) as new pattern
+        new_offset_pattern = np.unpackbits(new_offset_pattern.astype(np.uint8), axis=-1) # unpack to 8 bits
+        ofs_pattern = new_offset_pattern[:,-OffsetBitSize:] # select indices 1 ~ 7 bits (discard 0) as new pattern
 
         # predict accomp
         key_input = np_utils.to_categorical(key_pattern, num_classes=KeySize)
         key_input = np.reshape(key_input, (1, key_input.shape[0], key_input.shape[1]))
         prs_input = np_utils.to_categorical(prs_pattern, num_classes=PressSize)
         prs_input = np.reshape(prs_input, (1, prs_input.shape[0], prs_input.shape[1]))
-        offset_input = np.reshape(offset_pattern, (1, offset_pattern.shape[0], offset_pattern.shape[1]))
-
-        prediction_input = [key_input, prs_input, offset_input]
-        prediction = accomp_model.predict(prediction_input, verbose=0)
+        acp_input = np_utils.to_categorical(acp_pattern, num_classes=KeySize)
+        acp_input = np.reshape(acp_input, (1, acp_input.shape[0], acp_input.shape[1]))
+        ofs_input = np.reshape(ofs_pattern, (1, ofs_pattern.shape[0], ofs_pattern.shape[1]))
+        prediction = accomp_model.predict([key_input, prs_input, ofs_input, acp_input], verbose=0)
         key_prediction = np.reshape(prediction[0], (prediction[0].shape[1], prediction[0].shape[2]))
         prs_prediction = np.reshape(prediction[1], (prediction[1].shape[1], prediction[1].shape[2]))
-
+        
         for index, (key_onehot, prs_onehot) in enumerate(zip(key_prediction, prs_prediction)):
             key_index = np.argmax(key_onehot)
             prs_index = np.argmax(prs_onehot)
+            acp_pattern[index] = key_index
             accomp_output.append((KeyToPitch[key_index], prs_index))
 
     return melody_output, accomp_output
 
-def create_midi(melody_output, accomp_output, scale_name=None):
+def create_midi(melody_output, accomp_output, midi_name=None, scale_name=None):
     output_part1 = []
     output_part2 = []
-
-    print(melody_output)
+    # print(melody_output)
     offset = 0
     # create note and chord objects based on the values generated by the model
     for (pitch, press) in melody_output:
@@ -196,7 +247,7 @@ def create_midi(melody_output, accomp_output, scale_name=None):
         # increase offset each iteration so that notes do not stack
         offset += OffsetStep
 
-    print(accomp_output)
+    # print(accomp_output)
     offset = 0
     for (pitch, press) in accomp_output:
         if len(output_part2) > 0 and press != 0:
@@ -222,7 +273,10 @@ def create_midi(melody_output, accomp_output, scale_name=None):
     if scale_name:
         midi_stream = midi_util.to_major(midi_stream, scale_name)
 
-    midi_stream.write('midi', fp='test_output.mid')
+    if midi_name:
+        midi_stream.write("midi", fp= midi_name + ".mid")
+    else:
+        midi_stream.write("midi", fp="test_output.mid")
 
-    stream_player = midi.realtime.StreamPlayer(midi_stream)
-    stream_player.play()
+    # stream_player = midi.realtime.StreamPlayer(midi_stream)
+    # stream_player.play()
--- network.py
+++ network.py
@@ -1,27 +1,34 @@
 from keras.models import Sequential, Model
-from keras.layers import Activation, Concatenate, Dropout, LSTM, Dense, Input, TimeDistributed, RepeatVector
+from keras.layers import Activation, Concatenate, Dropout, LSTM, Dense, Input, TimeDistributed, RepeatVector, Bidirectional, Lambda
 from keras.callbacks import ModelCheckpoint
 import keras.backend as K
 import tensorflow as tf
 import numpy as np
 
-def create_molody_model(sequence_length, key_size, press_size, offset_size, weights_path=None):
+def create_melody_model(sequence_length, key_size, press_size, offset_size, weights_path=None):
     key_input = Input(shape=(sequence_length, key_size))
     press_input = Input(shape=(sequence_length, press_size))
     offset_input = Input(shape=(sequence_length, offset_size))
 
+    key_layer_input = Concatenate(axis=-1)([key_input, press_input, offset_input])
     key_layer = Sequential()
     key_layer.add(LSTM(512, return_sequences=True))
     key_layer.add(Dropout(0.2))
     key_layer.add(LSTM(256, return_sequences=True))
     key_layer.add(TimeDistributed(Dense(128)))
     key_layer.add(Dropout(0.2))
-    
-    key_layer_input = Concatenate(axis=-1)([key_input, press_input, offset_input])
     key_info = key_layer(key_layer_input)
     key_out = Dense(key_size)(key_info)
     key_out = Activation('softmax', name="key")(key_out)
-    press_out = Dense(press_size)(key_info)
+
+    press_layer = Sequential()
+    press_layer.add(LSTM(256, return_sequences=True))
+    press_layer.add(Dropout(0.2))
+    press_layer.add(LSTM(128, return_sequences=True))
+    press_layer.add(TimeDistributed(Dense(64)))
+    press_layer.add(Dropout(0.2))
+    press_info = press_layer(key_info)
+    press_out = Dense(press_size)(press_info)
     press_out = Activation('softmax', name="press")(press_out)
 
     losses = {
@@ -37,24 +44,31 @@ def create_molody_model(sequence_length, key_size, press_size, offset_size, weig
 
     return model
 
-
 def create_accomp_model(sequence_length, key_size, press_size, offset_size, weights_path=None):
     key_input = Input(shape=(sequence_length, key_size))
     press_input = Input(shape=(sequence_length, press_size))
     offset_input = Input(shape=(sequence_length, offset_size))
+    accomp_input = Input(shape=(sequence_length, key_size))
 
+    key2_layer_input = Concatenate(axis=-1)([key_input, press_input, offset_input, accomp_input])
     key2_layer = Sequential()
     key2_layer.add(LSTM(512, return_sequences=True))
-    key2_layer.add(Dropout(0.25))
+    key2_layer.add(Dropout(0.2))
     key2_layer.add(LSTM(256, return_sequences=True))
     key2_layer.add(TimeDistributed(Dense(128)))
-    key2_layer.add(Dropout(0.25))
-    
-    key2_layer_input = Concatenate(axis=-1)([key_input, press_input, offset_input])
+    key2_layer.add(Dropout(0.2))
     key2_info = key2_layer(key2_layer_input)
     key2_out = Dense(key_size)(key2_info)
     key2_out = Activation('softmax', name="key2")(key2_out)
-    press2_out = Dense(press_size)(key2_info)
+
+    press2_layer = Sequential()
+    press2_layer.add(LSTM(256, return_sequences=True))
+    press2_layer.add(Dropout(0.2))
+    press2_layer.add(LSTM(128, return_sequences=True))
+    press2_layer.add(TimeDistributed(Dense(64)))
+    press2_layer.add(Dropout(0.2))
+    press2_info = press2_layer(key2_info)
+    press2_out = Dense(press_size)(press2_info)
     press2_out = Activation('softmax', name="press2")(press2_out)
 
     losses = {
@@ -62,7 +76,7 @@ def create_accomp_model(sequence_length, key_size, press_size, offset_size, weig
         "press2" : "categorical_crossentropy"
     }
 
-    model = Model(inputs=[key_input, press_input, offset_input], outputs=[key2_out, press2_out])
+    model = Model(inputs=[key_input, press_input, offset_input, accomp_input], outputs=[key2_out, press2_out])
     model.compile(loss=losses, optimizer='rmsprop')
 
     if weights_path:
@@ -70,7 +84,7 @@ def create_accomp_model(sequence_length, key_size, press_size, offset_size, weig
 
     return model
 
-def train(model, epoch, data, target, model_name):
+def train(model, epoch, data, target, model_name, batch_size=1024):
     filepath = "weights/"+ model_name + "-"+ str(epoch) +"-{loss:.4f}.hdf5"
     checkpoint = ModelCheckpoint(
         filepath,
@@ -81,7 +95,7 @@ def train(model, epoch, data, target, model_name):
     )
     callbacks_list = [checkpoint]
 
-    model.fit(x=data, y=target, epochs=1, batch_size=512, callbacks=callbacks_list)
+    model.fit(x=data, y=target, epochs=1, batch_size=batch_size, callbacks=callbacks_list)
 
 def keys_binary_crossentropy(y_true, y_pred):
     press_true_mask = K.greater_equal(y_true, 0.5)
